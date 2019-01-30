0adfd78949f8e7c57bf99901135a0a28f4ba87a5
shoter <lubiebardzozelki@gmail.com>
2019-01-30 11:34:29 +0100

    Dependency hell - lite version

--- DependencyHell/Program.cs
+++ DependencyHell/Program.cs
@@ -1,7 +1,9 @@
-using InnocentLookingProject;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +14,12 @@ namespace DependencyHell
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Time to start something bad");
-            var version = typeof(X509Certificate).Assembly.GetName().Version;
-            Console.WriteLine(version);
-            InnocentClass.LetsStartAHell();
+            // We want to use version 4.1.1.3
+            // At my computer VS tells me it will use 4.2.0 but it does not copy assembly so it uses assembly from GAC with version of 4.0.0.0.
+            // Sense? What sense? There is no sense here!
+            Console.WriteLine(typeof(WebRequest).Assembly.Location);
+            Console.WriteLine(typeof(WebRequest).Assembly.GetName().FullName);
+            Console.WriteLine(typeof(WebRequest).Assembly.GetName().Version);
             Console.ReadKey();
         }
     }
