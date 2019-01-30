14e6f73ff89e5169035a6bed26479147ff7c4916
shoter <lubiebardzozelki@gmail.com>
2019-01-30 11:35:46 +0100

    Wrong lib

--- DependencyHell/Program.cs
+++ DependencyHell/Program.cs
@@ -1,12 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DependencyHell
 {
@@ -17,9 +11,9 @@ namespace DependencyHell
             // We want to use version 4.1.1.3
             // At my computer VS tells me it will use 4.2.0 but it does not copy assembly so it uses assembly from GAC with version of 4.0.0.0.
             // Sense? What sense? There is no sense here!
-            Console.WriteLine(typeof(WebRequest).Assembly.Location);
-            Console.WriteLine(typeof(WebRequest).Assembly.GetName().FullName);
-            Console.WriteLine(typeof(WebRequest).Assembly.GetName().Version);
+            Console.WriteLine(typeof(HttpContent).Assembly.Location);
+            Console.WriteLine(typeof(HttpContent).Assembly.GetName().FullName);
+            Console.WriteLine(typeof(HttpContent).Assembly.GetName().Version);
             Console.ReadKey();
         }
     }
